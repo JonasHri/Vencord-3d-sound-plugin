a7b80f8b0990031c35282129e91e8b29c055eb05
JonasHri <jonasharriehausen@gmail.com>
2025-06-02 12:08:06 +0200

    initial setup

---
+++ src/plugins/3dSound/index.ts
@@ -0,0 +1,14 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "3DSound",
+    description: "Adds 3D Sound to Discord",
+    authors: [Devs.JonasHri],
+});
